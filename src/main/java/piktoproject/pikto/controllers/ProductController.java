677fbd1321c549ad98d90d7b3d26ef09a2875ba7
Marcus Polland <marcus.polland@gmail.com>
2021-02-02 10:18:06 +0100

    ändra services

--- src/main/java/piktoproject/pikto/controllers/ProductController.java
+++ src/main/java/piktoproject/pikto/controllers/ProductController.java
@@ -4,13 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.handler.UserRoleAuthorizationInterceptor;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import piktoproject.pikto.service.AdminService;
+import piktoproject.pikto.services.AdminService;
 
 @Controller
 @SessionAttributes("userName")
