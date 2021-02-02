d7c673295b26d674ff9ad8b72be85f5c0a7c481a
Marcus Polland <marcus.polland@gmail.com>
2021-02-02 10:29:20 +0100

    awd

--- src/main/java/piktoproject/pikto/controllers/ProductController.java
+++ src/main/java/piktoproject/pikto/controllers/ProductController.java
@@ -4,14 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-<<<<<<< HEAD
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-=======
->>>>>>> 677fbd1321c549ad98d90d7b3d26ef09a2875ba7
 import piktoproject.pikto.services.AdminService;
 
 @Controller
