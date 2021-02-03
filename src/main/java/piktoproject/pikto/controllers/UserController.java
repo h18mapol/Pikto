843ee5a23fb7f6731e890b8cc22eb16bbc564be6
Marcus Polland <marcus.polland@gmail.com>
2021-02-03 12:40:24 +0100

    Fixat några models och lagt till några shopping functions

--- src/main/java/piktoproject/pikto/controllers/UserController.java
+++ src/main/java/piktoproject/pikto/controllers/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import piktoproject.pikto.services.AdminService;
 import piktoproject.pikto.services.UserService;
 
 @Controller
