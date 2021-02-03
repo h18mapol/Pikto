7e3a658dc540b4f6685950090526e041589184eb
Marcus Polland <marcus.polland@gmail.com>
2021-02-03 10:28:39 +0100

    Fixat några models och lagt till några shopping functions

--- src/main/java/piktoproject/pikto/controllers/UserController.java
+++ src/main/java/piktoproject/pikto/controllers/UserController.java
@@ -14,15 +14,15 @@ public class UserController {
     private UserService userService;
 
 
-    @RequestMapping("/User/{userId}/Products/")
-    public String getAdminPage(Model model, @PathVariable Integer userId){
+    @RequestMapping("/User/{userId}/Products")
+    public String getUserAllProducts(Model model, @PathVariable Integer userId){
         model.addAttribute("allProducts",userService.getAllProducts(userId));
         System.out.println(userService.getAllProducts(userId).get(0).getProductId()); //Funkar
         return "urlview";
     }
 
     @RequestMapping("/User/Product/{productId}")
-    public String getAllProducts(Model model, @PathVariable Integer productId){
+    public String getProduct(Model model, @PathVariable Integer productId){
         model.addAttribute("product",userService.getProduct(productId));
         System.out.println(userService.getProduct(productId).getProductId());
         return "getproduct";
@@ -30,19 +30,19 @@ public class UserController {
 
     @RequestMapping("/User/{userId}")
     public String getUserById(Model model, @PathVariable Integer userId){
-        model.addAttribute("user",userService.getProduct(userId));
+        model.addAttribute("user",userService.getUserById(userId));
         System.out.println(userService.getUserById(userId).getFirstName());
         return "getuser";
     }
 
-    @RequestMapping("/User/Reviews/{userId}")
+    @RequestMapping("/User/{userId}/Reviews")
     public String getAllReviews(Model model, @PathVariable Integer userId){
         model.addAttribute("reviews",userService.getAllReviews(userId));
         System.out.println(userService.getAllReviews(userId).get(0).getTitle());
         return "getreview";
     }
 
-    @RequestMapping("/User/Orders/{userId}")
+    @RequestMapping("/User/{userId}/Orders")
     public String getAllOrders(Model model, @PathVariable Integer userId){
         model.addAttribute("orders",userService.getAllOrders(userId));
         System.out.println(userService.getAllOrders(userId).get(0).getFirstName());
