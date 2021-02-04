e95576719a042b0c9b2f99a7111925de1cd0d5cd
Marcus Polland <marcus.polland@gmail.com>
2021-02-04 16:03:43 +0100

    Refactoring the interface and crud opperationer

--- src/main/java/piktoproject/pikto/controllers/UserController.java
+++ src/main/java/piktoproject/pikto/controllers/UserController.java
@@ -50,12 +50,7 @@ public class UserController {
         return "getorders";
     }
 
-    @RequestMapping("/User/{userId}/Orders")
-    public String getAllOrders(Model model, @PathVariable Integer userId){
-        model.addAttribute("orders",userService.getAllOrders(userId));
-        System.out.println(userService.getAllOrders(userId).get(0).getFirstName());
-        return "getorders";
-    }
+
 
 
 
