8ca47042a41a16d0c0640df12ba2efefb81c6e82
HenrikMeurke <henrik.meurke@hotmail.com>
2021-02-05 23:48:16 +0100

    hej

--- src/main/java/piktoproject/pikto/controllers/ProductController.java
+++ src/main/java/piktoproject/pikto/controllers/ProductController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import piktoproject.pikto.services.ShoppingService;
+import piktoproject.pikto.services.UserService;
 
 @Controller
 @SessionAttributes("userName")
@@ -12,11 +13,20 @@ public class ProductController {
 
  @Autowired
  private ShoppingService productService;
-
+@Autowired
+  private UserService userService;
     @RequestMapping("/login")
     public String loginUser(Model model){
         String userName = "Marcus";
         model.addAttribute("userName", userName);
         return "urlview2";
     }
+    
+    
+    @RequestMapping("/User/Checkout/{userId}")
+    public String checkoutUser(Model model, @PathVariable Integer userId){
+        model.addAttribute("user",userService.getUserById(userId));
+        return "Frontend/User/Checkout";
+    }
+    
 }
